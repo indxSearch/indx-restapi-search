58026575df555756c9efb9474840f9f80dd62d8a
Anders Tangen <anders@brutal.as>
2024-05-15 15:48:17 +0200

    title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Indx Search System / RestAPI Local demo',
+  title: 'Indx RestAPI Search',
   description: 'Advanced search with pattern recognition.',
   openGraph: {
     url: 'https://indx.co',
